0441acf1ac4d35faf80a9abede5fea33e1a05ce8
EJ Llovido <ericllovido08@gmail.com>
2024-02-29 00:14:17 +0800

    Implements banner

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -4,6 +4,8 @@ import { sample } from '../../utils'
 import { WORDS } from '../../data'
 import GuessInput from '../GuessInput'
 import GuessResult from '../GuessResult'
+import Banner from '../Banner'
+import { NUM_OF_GUESSES_ALLOWED } from '../../constants'
 
 // Pick a random word on every pageload.
 const answer = sample(WORDS)
@@ -11,17 +13,25 @@ const answer = sample(WORDS)
 console.info({ answer })
 
 function Game() {
+  const [gameStatus, setGameStatus] = React.useState('running')
   const [guesses, setGuesses] = React.useState([])
 
   const handleSubmitGuess = (tentativeGuess) => {
     const nextGuesses = [...guesses, tentativeGuess]
     setGuesses(nextGuesses)
+
+    if (tentativeGuess === answer) {
+      setGameStatus('won')
+    } else if (nextGuesses.length >= NUM_OF_GUESSES_ALLOWED) {
+      setGameStatus('lost')
+    }
   }
 
   return (
     <>
       <GuessResult guesses={guesses} answer={answer} />
       <GuessInput handleSubmitGuess={handleSubmitGuess} />
+      <Banner gameStatus={gameStatus} numOfGuesses={guesses.length} answer={answer} />
     </>
   )
 }
